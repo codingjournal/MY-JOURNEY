1b58cc25d4f7cbc119bb70c6bf9eac06e8dec01e
codingjournal <beginnerscode.6@gmail.com>
2026-09-07 19:55:34 +0530

    Solved Q 16

---
+++ JAVASCRIPT PRACTICE/Answer16/script.js
@@ -0,0 +1,6 @@
+let course = "Javascript";
+let duration = "06 Months";
+let goal = "Full Stack Developer";
+console.log("Course: ",course);
+console.log("Duration: ",duration);
+console.log("Goal: ",goal);
