d125c7ba564bd4fd398c15fb4a28d0e624f402d9
codingjournal <beginnerscode.6@gmail.com>
2026-09-10 21:41:03 +0530

    Solved Q25 and Q26

---
+++ JAVASCRIPT PRACTICE/Answer25/script.js
@@ -0,0 +1,22 @@
+let name = "Johny Singh";
+console.log(typeof name);
+
+let name1 = "Johny Mastan";
+console.log(name1);
+console.log("Type of ",name1," is ",typeof name1);
+
+let age = 43;;
+console.log(age);
+console.log("Type of ",age," is ",typeof age);
+
+let isStudent = true;
+console.log(isStudent);
+console.log("Type of ",isStudent," is ",typeof isStudent);
+
+let city;
+console.log(city);
+console.log("Type of ",city," is ",typeof city);
+
+let manager = null;
+console.log(manager);
+console.log("Type of ",manager," is ",typeof manager);
